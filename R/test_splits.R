04b4f6b5a73460572555eb8b37b29a150a247274
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-17 16:54:27 -0700

    try the possible splits at this node and report the utility of each

---
+++ R/test_splits.R
@@ -0,0 +1,12 @@
+#' Try the possible splits at this node
+test_splits <- function(splitfun, data, response, outcome, weights) {
+  
+  # compute the utility on each possible split
+  xselect <- which( names(data) != response )
+  utility <- sapply(xselect, function(i) splitfun( rep( data[[i]], weights ), outcome, weights ) )
+  names(utility) <- names(data)[ xselect ]
+  
+  # if there is a threshold adjustment specified then apply it, otherwise return the values from splitfun
+  utility
+  
+}
