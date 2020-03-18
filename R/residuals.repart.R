0fc7178bff013e9604326b87687ea40edd038f66
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-18 11:54:55 -0700

    updated documentation of residuals.repart method

--- R/residuals.repart.R
+++ R/residuals.repart.R
@@ -1,14 +1,18 @@
 #' Calculate residuals for a classification tree
 #' 
-#' @param node A classification tree
+#' @param node A classification tree fitted by repart
 #' @return A matrix of the classwise residuals
 #' @export
 residuals.repart <- function( tree, type = "logit" ) {
+  
+  # get the fitted values
   outcome <- model.matrix( ~ -1 + `(response)`, data = tree$fitted )
   fit <- fitted.tree( tree )
   
+  # calculate residuals as observed - fitted
   resid <- outcome - fit
   colnames( resid ) <- colnames( fit )
 
+  # return the residuals
   resid
 }
