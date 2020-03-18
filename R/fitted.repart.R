f859af2166a72baad9348bba7861a7853b8873df
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-18 11:49:25 -0700

    updated documentation for the fitted.repart method

--- R/fitted.repart.R
+++ R/fitted.repart.R
@@ -1,4 +1,11 @@
+#' Extract the fitted values from the tree model
+#' 
+#' @param tree The tree model from which to extract the fitted values
+#' @return The fitted values for the observations used to estimate the tree model
+#' @export
 fitted.repart <- function( tree ) {
+  
+  # recover the outcome factor from the input data
   outcome <- model.matrix( ~ -1 + `(response)`, data = tree$fitted )
   fitted <-  matrix( NA, nrow = nrow( outcome ), ncol = ncol( outcome ) )
   colnames( fitted ) <- levels(tree$fitted[[ "(response)" ]])
@@ -8,9 +15,10 @@ fitted.repart <- function( tree ) {
     indx <- tree$fitted[[ "(fitted)" ]] == node
     fit <- matrix( rep( colMeans(outcome[indx, ]), each=sum(indx) ), nrow=sum(indx) )
     
-    
+    # fill in the fitted values for the observations that ended up in this node
     fitted[indx,] <- fit
   }
   
+  # return the fitted outcomes
   fitted
 }
