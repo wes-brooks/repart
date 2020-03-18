1f3c2d9d6a7802f515d2f4009c976deced11a66f
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-18 12:04:19 -0700

    updated the documentation of chi_squared function

--- R/chi_squared.R
+++ R/chi_squared.R
@@ -1,15 +1,18 @@
 #' perform chi-squared test of y vs. x
-chi_squared <- function( x, outcome, weights ) {
+#' @param x A factor variable to be cross-tabulated with outcome
+#' @param outcome A matrix of binned observed categories
+#' @param weights A vector of prior weights
+#' @return Log p-value of the chi-squared statistic from the contingency table of x vs outcome.
+#' 
+chi_squared_factor <- function( x, outcome, weights ) {
   
-  x <- factor( x )
+  # fail if x is not a vector
+  if ( !is.factor(x) ) stop( "Must use chi_squared_factor with a factor variable x." )
+  
+  # return NA if x takes only one level among the variables in this node
   if ( length( levels(x) ) < 2 ) return( NA )
+  
+  # calculate the p-value of a chi-squared test for independence between x and outcome.
   ct <- suppressWarnings( chisq.repart( table.repart( outcome, x, weights ), correct = FALSE ) )
   pchisq( ct$statistic, ct$parameter, log = TRUE, lower.tail = FALSE )
-
-  # ## Bonferroni-adjusted p-value small enough?
-  # if ( all( is.na(logp) ) ) return( NULL )
-  # minp <- exp( min( logp, na.rm = TRUE ) )
-  # minp <- 1 - (1 - minp)^sum( !is.na( logp ) )
-  # 
-  # -minp
 }
