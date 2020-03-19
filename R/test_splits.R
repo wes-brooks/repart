dce89ca159fe7f575bcc22d4084ab9b045b11bb0
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-19 15:08:13 -0700

    return a list (rather than vector) of possible splits. The list preserves attributes of its elements, which we use to fund split points on continuous variables.

--- R/test_splits.R
+++ R/test_splits.R
@@ -9,7 +9,7 @@ test_splits <- function(splitfun, data, response, outcome, weights) {
   
   # compute the utility on each possible split
   xselect <- which( names(data) != response )
-  utility <- sapply(xselect, function(i) splitfun( rep( data[[i]], weights ), outcome, weights ) )
+  utility <- sapply(xselect, function(i) splitfun( rep( data[[i]], weights ), outcome, weights ), simplify = FALSE )
   names(utility) <- names(data)[ xselect ]
   
   # if there is a threshold adjustment specified then apply it, otherwise return the values from splitfun
