c470096fbeb5502e5b8d275cf1cdc1954d0188ac
Wesley Brooks <wbrooks@ucdavis.edu>
2020-03-19 15:11:41 -0700

    make entropy_factor work more generically

--- R/entropy_factor.R
+++ R/entropy_factor.R
@@ -6,23 +6,16 @@
 #' @return The information entropy of the binned variable
 #' @export
 entropy_factor <- function ( x, outcome, weights ) {
-  if (nrow(outcome) == 0) { 0 }
-  else {
-    
-    # begin by calculating the entropy if there is no split on this variable
-    cs <- colSums(outcome * weights)
-    entr_prior <- sum( ifelse( cs == 0, 0, cs * log2(cs / sum(cs)) ) )
-    
-    x <- factor( x )
-    if ( length( levels(x) ) < 2 ) return( NA )
-    counts <- table.repart( outcome, x, weights )
 
-    # calculate the entropy of the proposed split
-    entr_prop <- apply(counts, 2, function(col) {
-      ifelse( col == 0, 0, col * log2(col / sum(col)) )
-    })
-    
-    # return the improvement in entropy
-    entr_prior - sum( entr_prop )
-  }
+  # prepare the contingency table
+  if ( length( levels(x) ) < 2 ) return( NA )
+  counts <- table.repart( outcome, x, weights )
+
+  # calculate the entropy of the proposed split
+  entr_prop <- apply(counts, 2, function(col) {
+    ifelse( col == 0, 0, col * log2(col / sum(col)) )
+  })
+  
+  # return the sum of binned entropies
+  sum( entr_prop )
 }
